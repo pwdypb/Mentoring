6cba238c81da89e71cd6dd35508b890c6a3c97f5
sajti <sajtos.gyula@gmail.com>
2017-07-17 01:34:51 +0200

    [MNTR-1] Tweaked application class

--- src/main/java/com/artos/mentoring/MentoringApplication.java
+++ src/main/java/com/artos/mentoring/MentoringApplication.java
@@ -2,9 +2,10 @@ package com.artos.mentoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MentoringApplication {
+public class MentoringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MentoringApplication.class, args);
