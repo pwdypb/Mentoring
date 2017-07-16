862876901952692b9fdfb455c6a23925d80e601b
sajti <sajtos.gyula@gmail.com>
2017-07-16 23:04:37 +0200

    [MNTR-1] Added application class

---
+++ src/main/java/com/artos/mentoring/MentoringApplication.java
@@ -0,0 +1,12 @@
+package com.artos.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoringApplication.class, args);
+	}
+}
