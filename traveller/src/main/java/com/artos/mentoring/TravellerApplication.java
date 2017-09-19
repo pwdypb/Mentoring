5939d87021c63c47e47002d577345ecd5ebd0fe7
sajti12 <gyula_sajtos@epam.com>
2017-09-19 12:49:03 +0200

    [MNTR-21] Some minor improvements

--- traveller/src/main/java/com/artos/mentoring/TravellerApplication.java
+++ traveller/src/main/java/com/artos/mentoring/TravellerApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MentoringApplication extends SpringBootServletInitializer {
+public class TravellerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MentoringApplication.class, args);
+		SpringApplication.run(TravellerApplication.class, args);
 	}
 }
--- traveller/src/main/java/com/artos/mentoring/controller/TravellerCrudController.java
+++ traveller/src/main/java/com/artos/mentoring/controller/TravellerCrudController.java
@@ -4,8 +4,10 @@ import com.artos.mentoring.model.Traveler;
 import com.artos.mentoring.service.TravellerService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.NoSuchElementException;
 
@@ -13,7 +15,7 @@ import java.util.NoSuchElementException;
 /**
  * Created by Gyula_Sajtos on 2017-09-19.
  */
-@Controller
+@RestController
 @RequestMapping("/traveller")
 public class TravellerCrudController {
 
@@ -22,34 +24,30 @@ public class TravellerCrudController {
     @Autowired
     private TravellerService travellerService;
 
-    @GetMapping
-    @ResponseBody
+    @RequestMapping
     public Iterable<Traveler> getAllTravellers() {
         LOG.info("Retrieving all travellers");
         return travellerService.getAllTravellers();
     }
 
     @PostMapping("/get")
-    @ResponseBody
     public Traveler getTraveller(@RequestParam String email) {
         LOG.info(String.format("Get traveller by email (%s)", email));
         return travellerService.getTravellerByEmail(email).orElseThrow(NoSuchElementException::new);
     }
 
     @PostMapping("/create")
-    @ResponseBody
     public Traveler createTraveller(@RequestParam String email) {
         LOG.info(String.format("Create traveller with email (%s)", email));
         return travellerService.createTravellerWithEmail(email);
     }
 
     @PostMapping("/delete")
-    @ResponseBody
     public Long deleteTraveller(@RequestParam String email) {
         LOG.info(String.format("Delete traveller by email (%s)", email));
-        Long noOfdeletedTravellers = travellerService.deleteTravellerByEmail(email);
-        LOG.info("Deleted " + noOfdeletedTravellers);
-        return noOfdeletedTravellers;
+        Long noOfDeletedTravellers = travellerService.deleteTravellerByEmail(email);
+        LOG.info(String.format("Deleted %d traveller(s)", noOfDeletedTravellers));
+        return noOfDeletedTravellers;
     }
 
 }
