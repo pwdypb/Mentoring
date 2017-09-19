2cb6fce7ff01b2deae583c457f15703e097454a4
sajti12 <sajtos.gyula@gmail.com>
2017-09-19 14:24:10 +0200

    [MNTR-21] New service added

---
+++ trek/src/main/java/com/artos/trek/TrekApplication.java
@@ -0,0 +1,12 @@
+package com.artos.trek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekApplication.class, args);
+	}
+}
